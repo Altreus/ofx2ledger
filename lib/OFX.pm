45cb72127b7d009406bd75eb07f2b9040565d61c
Alastair McGowan-Douglas <alastair.mcgowan@opusvl.com>
2017-11-21 23:27:25 +0000

    It compiles...

--- lib/OFX.pm
+++ lib/OFX.pm
@@ -1,11 +1,12 @@
 use v6;
-use XML::Document;
+
+use XML;
+
+role OFX::TransferList {
+}
 
 role OFX {
     multi method transfer-list(XML::Document:) {
-        ($_ but OFX::TransferList for .lookfor(:TAG<BANKTRANLIST>, :SINGLE).nodes);
+        map { $_ but OFX::TransferList }, self.lookfor(:TAG<BANKTRANLIST>, :SINGLE).nodes;
     }
-}
-
-role OFX::TransferList {
 }
